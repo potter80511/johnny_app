3cf67528e0138004a52018a565ee9a51ab2b4eb6
potter80511@gmail.com <potter80511@gmail.com>
2024-01-29 23:00:41 +0800

    Create pringpringcats page

---
+++ src/features/pringpringcats/index.tsx
@@ -0,0 +1,7 @@
+import { CommonWrap } from "src/styles/Styled"
+
+const PringPringCatsIndex = () => {
+  return <CommonWrap>PringPringCatsIndex</CommonWrap>
+}
+
+export default PringPringCatsIndex
--- src/styles/Styled/index.ts
+++ src/styles/Styled/index.ts
@@ -3,4 +3,5 @@ import { styled } from 'styled-components';
 export const CommonWrap = styled.div`
   max-width: 1024px;
   width: 100%;
+  margin: 0 auto;
 `
