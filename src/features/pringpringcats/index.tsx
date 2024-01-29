152ed770e3df741f62cc01d662b087c554b4fd93
potter80511@gmail.com <potter80511@gmail.com>
2024-01-29 23:19:58 +0800

    Test Fetch pringpringcats chanel info

--- src/features/pringpringcats/index.tsx
+++ src/features/pringpringcats/index.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react"
 import { CommonWrap } from "src/styles/Styled"
 
 const PringPringCatsIndex = () => {
+  const fetchPringPringCats = async () => {
+    try {
+      const response = await fetch('https://www.googleapis.com/youtube/v3/channels?key=AIzaSyDrvBbitoDXcIHklICoeE6w_guJrwotF0k&id=UCrfpfIhOA_bH9QJvZNluv9w&part=snippet,contentDetails,statistics')
+      const rawData = await response.json()
+      console.log(rawData)
+    } catch(error) {
+      console.log(error, 'fetchPringPringCatsError')
+    }
+  }
+  useEffect(() => {
+    fetchPringPringCats()
+  }, [])
   return <CommonWrap>PringPringCatsIndex</CommonWrap>
 }
 
