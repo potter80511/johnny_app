621945b15e3baa4880a99499f4e174e6d4d1852a
potter80511@gmail.com <potter80511@gmail.com>
2024-01-30 23:14:05 +0800

    Create '/api/pringpringcats/channel' api route

---
+++ src/pages/api/pringpringcats/channel.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+const channel = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const response = await fetch(
+      'https://www.googleapis.com/youtube/v3/channels?key=AIzaSyDrvBbitoDXcIHklICoeE6w_guJrwotF0k&id=UCrfpfIhOA_bH9QJvZNluv9w&part=snippet,contentDetails,statistics',
+    )
+    console.log(response, 'response')
+    const rawData = await response.json()
+    res.json({ data: rawData })
+  } catch (error) {
+    throw (error)
+  }
+}
+
+export default channel
