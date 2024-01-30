5eaad165ef773e9cf21d15f7a71ecb6ee7afced2
potter80511@gmail.com <potter80511@gmail.com>
2024-01-30 23:37:26 +0800

    Add getServerSideProps

--- src/features/pringpringcats/index.tsx
+++ src/features/pringpringcats/index.tsx
@@ -2,17 +2,17 @@ import { useEffect } from "react"
 import { CommonWrap } from "src/styles/Styled"
 
 const PringPringCatsIndex = () => {
-  const fetchChannel = async () => {
-    try {
-      const response = await fetch('/api/pringpringcats/channel')
-      const rawData = await response.json()
-    } catch(error) {
-      console.log(error, 'fetchPringPringCatsError')
-    }
-  }
-  useEffect(() => {
-    fetchChannel()
-  }, [])
+  // const fetchChannel = async () => {
+  //   try {
+  //     const response = await fetch('/api/pringpringcats/channel')
+  //     const rawData = await response.json()
+  //   } catch(error) {
+  //     console.log(error, 'fetchPringPringCatsError')
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetchChannel()
+  // }, [])
   return <CommonWrap>PringPringCatsIndex</CommonWrap>
 }
 
