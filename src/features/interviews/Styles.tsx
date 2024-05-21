a79219fc38f7499b9ec218dbdcd2c7e04253a001
potter80511 <potter80511@gmail.com>
2024-05-21 10:07:58 +0800

    Update table styles

--- src/features/interviews/Styles.tsx
+++ src/features/interviews/Styles.tsx
@@ -3,15 +3,13 @@ import styled, { css } from 'styled-components'
 export const TableWrapper = styled.table`
   background: #171717;
   padding: 16px 0;
-  border-collapse: collapse; 
+  border-collapse: collapse;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, .117647), 0 1px 4px rgba(0, 0, 0, .117647);
 `
 export const TbodyTr = styled.tr`
   transition: all .3s;
-  border-top: 1px solid #272C34;
+  border-top: 1px solid #3e444e;
   padding: 4px 0;
-  &:hover {
-    background-color: #272C34;
-  }
 `
 const baseCell = css`
   padding: 8px;
