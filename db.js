1589ced1954b019166335eb5144c49c725596b94
potter80511 <potter80511@gmail.com>
2024-06-05 16:26:30 +0800

    Create dockerfile settings

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 // 建立 MySQL 連接池
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'mydatabase',
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'mydatabase',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
