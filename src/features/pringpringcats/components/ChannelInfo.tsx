20d999c43c5252650f9d7b8d1088a81d5d8eb814
potter80511@gmail.com <potter80511@gmail.com>
2024-06-09 00:32:35 +0800

    Add description

--- src/features/pringpringcats/components/ChannelInfo.tsx
+++ src/features/pringpringcats/components/ChannelInfo.tsx
@@ -8,17 +8,21 @@ const AvatarWrapper = styled.div`
   overflow: hidden;
   width: 160px;
   margin-right: 16px;
+  align-self: flex-start;
 `
 
 const ChannelTitle = styled.h2`
   font-size: 32px;
   margin-top: 0;
+  color: white;
 `
 const StatisticsWrapper = styled.div`
-  color: #aaa;
   a {
     color: #3ea6ff;
   }
+`
+const Right = styled.div`
+  color: #aaa;
 `
 
 const ChannelInfo = ({
@@ -29,17 +33,18 @@ const ChannelInfo = ({
   const {
     thumbnails,
     customUrl,
-    localized: { title }
+    localized: { title, description }
   } = snippet
 
   return <Flex>
     <AvatarWrapper><img src={thumbnails.medium.url} alt="pringpringcats" /></AvatarWrapper>
-    <div>
+    <Right>
       <ChannelTitle>{title}</ChannelTitle>
       <StatisticsWrapper>
         <a href={`https://www.youtube.com/channel/${id}`} target="_blank">{customUrl}</a> ‧ {statistics.subscriberCount}位訂閱者 ‧ {statistics.videoCount} 部影片 ‧ {convertUnit(statistics.viewCount)} 觀看次數
       </StatisticsWrapper>
-    </div>
+      <p>{description}</p>
+    </Right>
   </Flex>
 }
 
