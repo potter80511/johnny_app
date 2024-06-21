ebd1280975bd073e1493ad1dc6ed3d0cdb2eba91
potter80511 <potter80511@gmail.com>
2024-06-21 10:51:59 +0800

    Add React hook form to CreateInterviewForm

--- src/features/interviews/components/forms/CreateInterviewForm.tsx
+++ src/features/interviews/components/forms/CreateInterviewForm.tsx
@@ -1,6 +1,10 @@
 import { Button, TextField } from "@mui/material"
 import { useState } from "react"
 import styled from "styled-components"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Interview } from "src/features/interviews/types"
+
+type Form = Pick<Interview, 'companyName'>
 
 const Form = styled.form`
   max-width: 500px;
@@ -12,21 +16,40 @@ const ButtonWrapper = styled.div`
 `
 
 const CreateInterviewForm = ({ onSubmit }: {
-  onSubmit: (data: string) => void
+  onSubmit: (data: Form) => void
 }) => {
-  const [companyName, setCompanyName] = useState('')
-  return <Form>
+  const {
+    control,
+    handleSubmit: handleRHFSubmit,
+    register,
+    clearErrors,
+    getValues,
+    formState: { errors, isValid },
+  } = useForm<Form>({
+    defaultValues: { companyName: '' },
+  })
+
+  const handleSubmit: SubmitHandler<Form> = (formData: Form) => {
+    console.log(formData, 'formData')
+    onSubmit(formData)
+  }
+
+  return <Form onSubmit={handleRHFSubmit(handleSubmit)}>
     <div>
       <TextField
         variant="filled"
         label="公司名稱"
-        value={companyName}
-        onChange={(e) => setCompanyName(e.target.value)}
         fullWidth
+        error={!!errors?.companyName}
+        helperText={errors?.companyName?.message || ''}
+        {...register('companyName', {
+          required: { value: true, message: '必填' },
+          minLength: {value: 2, message: '字數至少2個字'},
+        })}
       />
     </div>
     <ButtonWrapper>
-      <Button variant="contained" onClick={() => onSubmit(companyName)}>送出</Button>
+      <Button variant="contained" type="submit">送出</Button>
     </ButtonWrapper>
   </Form>
 }
