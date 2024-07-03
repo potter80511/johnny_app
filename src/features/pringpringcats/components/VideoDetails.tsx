cb8964e68cb6cc6b904e7da69cb6cbcaa970469f
potter80511 <potter80511@gmail.com>
2024-07-03 09:10:57 +0800

    Remove useless code

--- src/features/pringpringcats/components/VideoDetails.tsx
+++ src/features/pringpringcats/components/VideoDetails.tsx
@@ -1,12 +1,5 @@
-import Flex from "src/components/Flex"
 import { YoutubeVideo } from "src/features/pringpringcats/types"
-import styled from "styled-components"
 import Popular from "src/features/pringpringcats/components/Popular"
-
-const Wrapper = styled.div`
-  /* max-width: 768px; */
-  width: 100%;
-`
 
 const VideoDetails = ({ details }: {details: YoutubeVideo | null}) => {
   if(!details) {
