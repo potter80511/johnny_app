9e69bcc49ea7300c9aa5ae80d29a8683dc3a3a80
potter80511 <potter80511@gmail.com>
2024-07-17 16:11:43 +0800

    Create PageLoading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { Provider } from 'react-redux';
 import { store } from 'src/store';
 import { ThemeProvider as MUIThemeProvider, createTheme as createMUITheme } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import PageLoading from 'src/components/PageLoading';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -41,12 +44,35 @@ export default function App({ Component, pageProps }: AppProps) {
   console.log(process.env.NEXT_PUBLIC_SITE_URL, 'NEXT_PUBLIC_SITE_URL')
   console.log(process.env.VERCEL_ENV, 'VERCEL_ENV')
   console.log(process.env.NODE_ENV, 'NODE_ENV')
+
+  const [isPageLoading, setIsPageLoading] = useState(false);
+
+  useEffect(() => {
+    const routeEventStart = () => {
+        setIsPageLoading(true);
+    };
+    const routeEventEnd = () => {
+        setIsPageLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', routeEventStart);
+    Router.events.on('routeChangeComplete', routeEventEnd);
+    Router.events.on('routeChangeError', routeEventEnd);
+    return () => {
+        Router.events.off('routeChangeStart', routeEventStart);
+        Router.events.off('routeChangeComplete', routeEventEnd);
+        Router.events.off('routeChangeError', routeEventEnd);
+    };
+  }, []);
+
+
   return (
     <>
       <GlobalStyle />
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <MUIThemeProvider theme={muiDarkTheme}>
+            {isPageLoading && <PageLoading />}
             <Component {...pageProps} />
           </MUIThemeProvider>
         </ThemeProvider>
