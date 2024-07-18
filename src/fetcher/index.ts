07ccd2dfebb0fb17263b92934530f55c95410a26
potter80511 <potter80511@gmail.com>
2024-07-18 17:13:07 +0800

    Update fetcher params type

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -1,8 +1,8 @@
-const fetcher = async (url: string, init?: RequestInit) => {
+const fetcher = async <RawData = any>(url: string, init?: RequestInit) => {
   try {
     const apiPath = process.env.NEXT_PUBLIC_SITE_URL + '/api' + url;
     const response = await fetch(apiPath, init)
-    const rawData = await response.json()
+    const rawData = await response.json() as APIResponse<RawData>
 
     return rawData
   } catch(error) {
