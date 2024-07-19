49313754489afea3b8013c38415c8bee1057cfae
potter80511 <potter80511@gmail.com>
2024-07-19 10:23:26 +0800

    Create fetchToLogin

--- src/features/common/users/fetchers/index.ts
+++ src/features/common/users/fetchers/index.ts
@@ -1,5 +1,7 @@
-import { RegisterUserPayload } from "src/features/common/users/types/net"
+import { LoginUserPayload, RawUser, RegisterUserPayload } from "src/features/common/users/types/net"
 import fetcher from "src/fetcher"
+import { User } from "src/features/common/users/types"
+import { createUserFromNet } from "src/features/common/users/factories"
 
 export const fetchToRegister: Fetcher<{ message: string }, RegisterUserPayload> = async (params) => {
   const { inputData: payload, callBack } = params
@@ -9,6 +11,33 @@ export const fetchToRegister: Fetcher<{ message: string }, RegisterUserPayload> 
 
     if(response.success) {
       callBack.onSuccess({message: response.message})
+    } else {
+      console.log('else')
+      callBack.onError({message: response.message})
+    }
+  } catch(error) {
+    console.log(error, 'error')
+    callBack.onError({message: error as string})
+  }
+}
+
+export const fetchToLogin: Fetcher<{
+  message: string,
+  data: { user: User, token: string}
+}, LoginUserPayload> = async (params) => {
+  const { inputData: payload, callBack } = params
+  
+  try {
+    const response = await fetcher<{ user: RawUser, token: string}>(`/users/login`, { method: 'POST', body: JSON.stringify(payload) })
+
+    if(response.success) {
+      callBack.onSuccess({
+        message: response.message,
+        data: {
+          user: createUserFromNet(response.data.user),
+          token: response.data.token
+        }
+      })
     } else {
       console.log('else')
       callBack.onError({message: response.message})
