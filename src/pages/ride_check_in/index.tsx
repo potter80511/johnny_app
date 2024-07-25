d5aae6aa319893c42e92e0b51548400f29a31679
potter80511 <potter80511@gmail.com>
2024-07-25 11:12:57 +0800

    Add meta data

--- src/pages/ride_check_in/index.tsx
+++ src/pages/ride_check_in/index.tsx
@@ -3,40 +3,34 @@ import MainLayout from 'src/components/MainLayout';
 import RideCheckInIndex from 'src/features/ride_check_in';
 import Head from 'next/head';
 
-// const meta = {
-//   title: "Johnny's App - 節拍器 Metronome",
-//   description: "Johnny's App - 節拍器 Metronome",
-//   keywords: "Johnny's App - 節拍器 Metronome",
-//   ogtitle: "Johnny's App - 節拍器 Metronome",
-//   ogdescription: "Johnny's App - 節拍器 Metronome",
-//   ogtype: 'website',
-//   ogimage: '',
-//   ogsitename: "Johnny's App - 節拍器 Metronome",
-//   ogurl: '',
-// };
+const meta = {
+  title: "Johnny's App - 共乘打卡",
+  description: "Johnny's App - 共乘打卡管理系統",
+  keywords: "共乘, 打卡",
+  ogtype: 'website',
+  ogimage: '',
+  ogurl: '',
+};
 
 const RideCheckin = () => {
-  // const {
-  //   title,
-  //   description,
-  //   keywords,
-  //   ogtitle,
-  //   ogdescription,
-  //   ogtype,
-  //   ogsitename
-  // } = meta
+  const {
+    title,
+    description,
+    keywords,
+    ogtype,
+  } = meta
 
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
-        <meta name="og:title" content={ogtitle} />
-        <meta name="og:description" content={ogdescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
         <meta name="og:type" content={ogtype} />
-        <meta name="og:sitename" content={ogsitename} />
-      </Head> */}
+        <meta name="og:sitename" content={title} />
+      </Head>
       <MainLayout id="ride-check-in">
         <RideCheckInIndex/>
       </MainLayout>
