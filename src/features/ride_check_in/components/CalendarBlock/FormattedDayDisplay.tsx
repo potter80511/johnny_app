e7c95069913524414fdaefb4eac27b2d86c00117
potter80511 <potter80511@gmail.com>
2024-07-31 17:09:00 +0800

    Fix isActive prop name

--- src/features/ride_check_in/components/CalendarBlock/FormattedDayDisplay.tsx
+++ src/features/ride_check_in/components/CalendarBlock/FormattedDayDisplay.tsx
@@ -2,9 +2,9 @@ import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
 import { Dayjs } from "dayjs";
 import styled from 'styled-components';
 
-const Wrapper = styled.div<{isActive: boolean}>`
+const Wrapper = styled.div<{$isActive: boolean}>`
   button {
-    color: ${({theme:{ palette }, isActive}) => isActive && palette.checkGreen};
+    color: ${({theme:{ palette }, $isActive}) => $isActive && palette.checkGreen};
   }
 `
 function FormattedDayDisplay(props: PickersDayProps<Dayjs> & {
@@ -21,7 +21,7 @@ function FormattedDayDisplay(props: PickersDayProps<Dayjs> & {
 
   const isActive = highlightedDays.indexOf(props.day.format('YYYY-MM-DD')) >= 0;
 
-  return <Wrapper isActive={isActive}>
+  return <Wrapper $isActive={isActive}>
     <PickersDay
       {...other}
       outsideCurrentMonth={outsideCurrentMonth}
